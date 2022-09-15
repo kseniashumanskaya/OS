1b3508d709687536f05bea95c1ee420162043962
KseniaShumanskaya <KseniaShumanskaya@gmail.com>
2022-09-15 21:35:12 +0300

    Create numbers.cpp

---
+++ lab1/numbers/numbers.cpp
@@ -0,0 +1,44 @@
+﻿// numbers.cpp : Определяет функции для статической библиотеки.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "numbers.h"
+
+	const Number ONE = 1;
+	const Number ZERO = 0;
+
+	Number Number::operator+(const Number& number2)
+	{
+		return Number(number_ + number2.number_);
+	}
+
+	Number Number::operator-(const Number& number2)
+	{
+		return Number(number_ - number2.number_);
+	}
+
+	Number Number::operator*(const Number& number2)
+	{
+		return Number(number_ * number2.number_);
+	}
+
+	Number Number::operator/(const Number& number2)
+	{
+		return Number(number_ / number2.number_);
+	}
+
+	Number Number::CreateNumber(int number)
+	{
+		return Number(number);
+	}
+
+	int Number::GetNumber()
+	{
+		return number_;
+	}
+
+	void Number::Print()
+	{
+		std::cout << number_;
+	}
