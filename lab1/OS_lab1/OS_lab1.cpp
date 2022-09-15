66185a9ac4d4fb355c189a7bb716502d9fd99f2c
KseniaShumanskaya <KseniaShumanskaya@gmail.com>
2022-09-15 21:38:15 +0300

    Create OS_lab1.cpp

---
+++ lab1/OS_lab1/OS_lab1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include "numbers.h"
+
+int main()
+{
+    Number two = Number(2);
+    Number seven = Number(7);
+    Number number1 = seven / two;
+    Number number2 = seven + two;
+    Number number3 = seven * two;
+    Number number4 = seven - two;
+    number1.Print();
+    std::cout << " ";
+    number2.Print();
+    std::cout << " ";
+    number3.Print();
+    std::cout << " ";
+    number4.Print();
+}
